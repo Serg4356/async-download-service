b5b134a8f38c860172b138f121584aaa4c5337ad
Serg4356 <asdfg111000@yandex.ru>
2019-05-21 16:56:54 +0500

    Make archivate function, make streaming

--- server.py
+++ server.py
@@ -1,9 +1,74 @@
 from aiohttp import web
 import aiofiles
+import asyncio
+import datetime
+import os
+import logging
+from multidict import CIMultiDict
 
 
-async def archivate(request):
-    raise NotImplementedError
+INTERVAL_SECS = 1
+
+
+async def uptime_handler(request):
+
+    response = web.StreamResponse()
+
+    # Большинство браузеров не отрисовывают частично загруженный контент,
+    # только если это не HTML.
+    # Поэтому отправляем клиенту именно HTML, указываем это в Content-Type.
+    response.headers['Content-Type'] = 'text/html'
+
+    # Отправляет клиенту HTTP заголовки
+    await response.prepare(request)
+
+    while True:
+        formatted_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        message = f'{formatted_date}<br>'  # <br> — HTML тег переноса строки
+
+        # Отправляет клиенту очередную порцию ответа
+        await response.write(message.encode('utf-8'))
+
+        await asyncio.sleep(INTERVAL_SECS)
+
+
+async def stream_archivation(request):
+    response = web.StreamResponse()
+    response.headers['Content-Type'] = 'application/zip'
+    response.headers['Content-Disposition'] = 'attachment; filename="archive_1.zip"'
+    request_folder = request.path.split('/')[-2]
+    if not request_folder in os.listdir('test_photos'):
+        raise web.HTTPNotFound(body=('404 ERROR.\nRequested archive doesnt '
+                                     'exist or was deleted'))
+    await response.prepare(request)
+    path = os.path.join('test_photos', request_folder)
+    filenames = get_filenames(path)
+    print(type(request))
+    async for chunk in archivate(filenames):
+        if request.transport.is_closing():
+            break
+        logging.debug(response.status)
+        await response.write(chunk)
+        await asyncio.sleep(1)
+    return response
+
+
+def get_filenames(path):
+    return ' '.join([os.path.join(path, filename) for filename in os.listdir(path)])
+
+
+async def archivate(filenames):
+    args = 'zip -r - ' + filenames
+    archive_binary = ''
+    archivate = await asyncio.create_subprocess_shell(args,
+                                                      stdout=asyncio.subprocess.PIPE,
+                                                      limit=800)
+    while True:
+        chunk = await archivate.stdout.read(800)
+        if not chunk:
+            break
+        logging.debug('Sending archive chunk...')
+        yield chunk
 
 
 async def handle_index_page(request):
@@ -12,10 +77,21 @@ async def handle_index_page(request):
     return web.Response(text=index_contents, content_type='text/html')
 
 
+async def write_to_file():
+    with open('archive.zip', 'ab') as archive:
+        async for chunk in  archivate():
+            archive.write(chunk)
+
+
 if __name__ == '__main__':
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
-        web.get('/archive/{archive_hash}/', archivate),
+        web.get('/archive/{archive_hash}/', stream_archivation),
     ])
     web.run_app(app)
+    #loop = asyncio.get_event_loop()
+    #loop.run_until_complete(write_to_file())
+    #loop.close()
