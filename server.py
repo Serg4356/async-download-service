19a03768135e36bdc0c932b1428dd729d9d76924
Serg4356 <asdfg111000@yandex.ru>
2019-06-04 18:09:12 +0500

    Finally add process killing

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import aiofiles
 import asyncio
 import datetime
 import os
+import signal
 import logging
 from multidict import CIMultiDict
 
@@ -32,10 +33,16 @@ async def uptime_handler(request):
         await asyncio.sleep(INTERVAL_SECS)
 
 
+async def on_shutdown(app):
+    for ws in set(app['websockets']):
+        await ws.close(code=WSCloseCode.GOING_AWAY,
+                       message='Server shutdown')
+
+
 async def stream_archivation(request):
     response = web.StreamResponse()
     response.headers['Content-Type'] = 'application/zip'
-    response.headers['Content-Disposition'] = 'attachment; filename="archive_1.zip"'
+    response.headers['Content-Disposition'] = 'attachment; filename="archive.zip"'
     request_folder = request.path.split('/')[-2]
     if not request_folder in os.listdir('test_photos'):
         raise web.HTTPNotFound(body=('404 ERROR.\nRequested archive doesnt '
@@ -43,42 +50,34 @@ async def stream_archivation(request):
     await response.prepare(request)
     path = os.path.join('test_photos', request_folder)
     filenames = get_filenames(path)
+    args = 'zip -r - ' + filenames
+    process = await asyncio.create_subprocess_shell(args,
+                                                    stdout=asyncio.subprocess.PIPE,
+                                                    limit=800,
+                                                    preexec_fn=os.setsid)
     try:
-        async for chunk in archivate(filenames):
+        async for chunk in archivate(filenames, process):
             await response.write(chunk)
             await asyncio.sleep(1)
         return response
     except asyncio.CancelledError as err:
-        print('Caught cancelled error from stream archivation')
-        return
-
-
-
+        print('Caught CancelledError from stream archivation')
+        os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+        raise
+    finally:
+        response.force_close()
 
 
 def get_filenames(path):
     return ' '.join([os.path.join(path, filename) for filename in os.listdir(path)])
 
 
-async def archivate(filenames):
-    args = 'zip -r - ' + filenames
-    archive_binary = ''
-    archivate = await asyncio.create_subprocess_shell(args,
-                                                      stdout=asyncio.subprocess.PIPE,
-                                                      limit=800)
-    try:
-        while True:
-            chunk = await archivate.stdout.read(800)
-            if not chunk:
-                break
-            logging.debug('Sending archive chunk...')
-            yield chunk
-    except asyncio.CancelledError as err:
-        print('Caught Cancelled Error')
-        logging.error(err)
-        archivate.terminate()
-        raise
-
+async def archivate(filenames, archivation_process):
+    while True:
+        chunk = await archivation_process.stdout.read(800)
+        if not chunk:
+            break
+        yield chunk
 
 
 async def handle_index_page(request):
@@ -101,7 +100,13 @@ if __name__ == '__main__':
         web.get('/', handle_index_page),
         web.get('/archive/{archive_hash}/', stream_archivation),
     ])
-    web.run_app(app)
-    #loop = asyncio.get_event_loop()
+    app.on_shutdown.append(on_shutdown)
+    try:
+        web.run_app(app)
+    except Exception as err:
+        print('Caught RuntimeError')
+    finally:
+        loop = asyncio.get_event_loop()
+        loop.close()
     #loop.run_until_complete(write_to_file())
     #loop.close()
